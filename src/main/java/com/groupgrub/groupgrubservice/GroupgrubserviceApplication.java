7e54d574adc2089a74fb3f0f3d5a53769740ada8
Zhengwen Fu <zfmyac@gmail.com>
2024-10-01 20:27:42 -0400

    init

---
+++ src/main/java/com/groupgrub/groupgrubservice/GroupgrubserviceApplication.java
@@ -0,0 +1,13 @@
+package com.groupgrub.groupgrubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupgrubserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupgrubserviceApplication.class, args);
+	}
+
+}
