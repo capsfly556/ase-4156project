b88c7bf2014aa0cae684913d803a38f185898b27
Chunyu <chunyusui@outlook.com>
2024-10-16 14:20:53 -0400

    feat: add java doc for GroupOrder.java

--- src/main/java/org/openapitools/model/GroupOrder.java
+++ src/main/java/org/openapitools/model/GroupOrder.java
@@ -17,9 +17,9 @@ import org.hibernate.annotations.Type;
 
 /** GroupOrder */
 @Generated(
-    value = "org.openapitools.codegen.languages.SpringCodegen",
-    date = "2024-10-05T01:59:00.934263-04:00[America/Toronto]",
-    comments = "Generator version: 7.8.0")
+        value = "org.openapitools.codegen.languages.SpringCodegen",
+        date = "2024-10-05T01:59:00.934263-04:00[America/Toronto]",
+        comments = "Generator version: 7.8.0")
 @Entity
 public class GroupOrder {
 
@@ -39,15 +39,22 @@ public class GroupOrder {
   @Column(nullable = false)
   private UUID foodProviderID;
 
+  /**
+   * Sets the groupOrderID of the GroupOrder and returns the updated GroupOrder object.
+   *
+   * @param groupOrderID A {@code UUID} representing the unique ID of the group order.
+   *
+   * @return The updated {@code GroupOrder} object with the specified groupOrderID.
+   */
   public GroupOrder groupOrderID(UUID groupOrderID) {
     this.groupOrderID = groupOrderID;
     return this;
   }
 
   /**
-   * Get groupOrderID
+   * Retrieves the groupOrderID of the GroupOrder.
    *
-   * @return groupOrderID
+   * @return A {@code UUID} representing the unique ID of the group order.
    */
   @Valid
   @Schema(name = "groupOrderID", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
@@ -56,20 +63,31 @@ public class GroupOrder {
     return groupOrderID;
   }
 
-
+  /**
+   * Sets the groupOrderID of the GroupOrder.
+   *
+   * @param groupOrderID A {@code UUID} representing the unique ID of the group order.
+   */
   public void setGroupOrderID(UUID groupOrderID) {
     this.groupOrderID = groupOrderID;
   }
 
+  /**
+   * Sets the status of the GroupOrder and returns the updated GroupOrder object.
+   *
+   * @param status A {@code String} representing the current status of the group order.
+   *
+   * @return The updated {@code GroupOrder} object with the specified status.
+   */
   public GroupOrder status(String status) {
     this.status = status;
     return this;
   }
 
   /**
-   * Get status
+   * Retrieves the status of the GroupOrder.
    *
-   * @return status
+   * @return A {@code String} representing the status of the group order.
    */
   @Schema(name = "status", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   @JsonProperty("status")
@@ -77,15 +95,34 @@ public class GroupOrder {
     return status;
   }
 
+  /**
+   * Sets the status of the GroupOrder.
+   *
+   * @param status A {@code String} representing the status of the group order.
+   */
   public void setStatus(String status) {
     this.status = status;
   }
 
+  /**
+   * Sets the list of participant order IDs for the GroupOrder and returns the updated object.
+   *
+   * @param participantOrderIDs A {@code List} of {@code UUID} objects representing participant order IDs.
+   *
+   * @return The updated {@code GroupOrder} object with the specified participant order IDs.
+   */
   public GroupOrder participantOrderIDs(List<UUID> participantOrderIDs) {
     this.participantOrderIDs = participantOrderIDs;
     return this;
   }
 
+  /**
+   * Adds a participant order ID to the GroupOrder's participant order list.
+   *
+   * @param participantOrderIDsItem A {@code UUID} representing a participant's order ID to add.
+   *
+   * @return The updated {@code GroupOrder} object with the added participant order ID.
+   */
   public GroupOrder addParticipantOrderIDsItem(UUID participantOrderIDsItem) {
     if (this.participantOrderIDs == null) {
       this.participantOrderIDs = new ArrayList<>();
@@ -95,9 +132,9 @@ public class GroupOrder {
   }
 
   /**
-   * Get participantOrderIDs
+   * Retrieves the list of participant order IDs for the GroupOrder.
    *
-   * @return participantOrderIDs
+   * @return A {@code List} of {@code UUID} representing the participant order IDs.
    */
   @Valid
   @Schema(name = "participantOrderIDs", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
@@ -106,19 +143,31 @@ public class GroupOrder {
     return participantOrderIDs;
   }
 
+  /**
+   * Sets the list of participant order IDs for the GroupOrder.
+   *
+   * @param participantOrderIDs A {@code List} of {@code UUID} representing the participant order IDs.
+   */
   public void setParticipantOrderIDs(List<UUID> participantOrderIDs) {
     this.participantOrderIDs = participantOrderIDs;
   }
 
+  /**
+   * Sets the desired pickup timeframe for the GroupOrder and returns the updated object.
+   *
+   * @param desiredPickupTimeframe A {@code String} representing the desired pickup timeframe for the group order.
+   *
+   * @return The updated {@code GroupOrder} object with the specified pickup timeframe.
+   */
   public GroupOrder desiredPickupTimeframe(String desiredPickupTimeframe) {
     this.desiredPickupTimeframe = desiredPickupTimeframe;
     return this;
   }
 
   /**
-   * Get desiredPickupTimeframe
+   * Retrieves the desired pickup timeframe for the GroupOrder.
    *
-   * @return desiredPickupTimeframe
+   * @return A {@code String} representing the desired pickup timeframe.
    */
   @Schema(name = "desiredPickupTimeframe", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
   @JsonProperty("desiredPickupTimeframe")
@@ -126,19 +175,31 @@ public class GroupOrder {
     return desiredPickupTimeframe;
   }
 
+  /**
+   * Sets the desired pickup timeframe for the GroupOrder.
+   *
+   * @param desiredPickupTimeframe A {@code String} representing the desired pickup timeframe.
+   */
   public void setDesiredPickupTimeframe(String desiredPickupTimeframe) {
     this.desiredPickupTimeframe = desiredPickupTimeframe;
   }
 
+  /**
+   * Sets the food provider ID for the GroupOrder and returns the updated object.
+   *
+   * @param foodProviderID A {@code UUID} representing the food provider's ID.
+   *
+   * @return The updated {@code GroupOrder} object with the specified food provider ID.
+   */
   public GroupOrder foodProviderID(UUID foodProviderID) {
     this.foodProviderID = foodProviderID;
     return this;
   }
 
   /**
-   * Get foodProviderID
+   * Retrieves the food provider ID for the GroupOrder.
    *
-   * @return foodProviderID
+   * @return A {@code UUID} representing the food provider's ID.
    */
   @Valid
   @Schema(name = "foodProviderID", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
@@ -147,25 +208,40 @@ public class GroupOrder {
     return foodProviderID;
   }
 
+  /**
+   * Sets the food provider ID for the GroupOrder.
+   *
+   * @param foodProviderID A {@code UUID} representing the food provider's ID.
+   */
   public void setFoodProviderID(UUID foodProviderID) {
     this.foodProviderID = foodProviderID;
   }
 
-  public void fillFields(){
-    if (groupOrderID==null){
-      groupOrderID=UUID.randomUUID();
+  /**
+   * Fills default values for groupOrderID, status, desiredPickupTimeframe, and foodProviderID if they are null.
+   */
+  public void fillFields() {
+    if (groupOrderID == null) {
+      groupOrderID = UUID.randomUUID();
     }
-    if (status==null){
+    if (status == null) {
       status = "status";
     }
-    if (desiredPickupTimeframe==null){
-      desiredPickupTimeframe="desiredPickupTimeframe";
+    if (desiredPickupTimeframe == null) {
+      desiredPickupTimeframe = "desiredPickupTimeframe";
     }
-    if (foodProviderID==null){
-      foodProviderID=UUID.randomUUID();
+    if (foodProviderID == null) {
+      foodProviderID = UUID.randomUUID();
     }
   }
 
+  /**
+   * Compares the current GroupOrder object with another object for equality.
+   *
+   * @param o The object to be compared.
+   *
+   * @return {@code true} if the objects are equal, {@code false} otherwise.
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -176,41 +252,47 @@ public class GroupOrder {
     }
     GroupOrder groupOrder = (GroupOrder) o;
     return Objects.equals(this.groupOrderID, groupOrder.groupOrderID)
-        && Objects.equals(this.status, groupOrder.status)
-        && Objects.equals(this.participantOrderIDs, groupOrder.participantOrderIDs)
-        && Objects.equals(this.desiredPickupTimeframe, groupOrder.desiredPickupTimeframe)
-        && Objects.equals(this.foodProviderID, groupOrder.foodProviderID);
+            && Objects.equals(this.status, groupOrder.status)
+            && Objects.equals(this.participantOrderIDs, groupOrder.participantOrderIDs)
+            && Objects.equals(this.desiredPickupTimeframe, groupOrder.desiredPickupTimeframe)
+            && Objects.equals(this.foodProviderID, groupOrder.foodProviderID);
   }
 
+  /**
+   * Generates the hash code for the current GroupOrder object.
+   *
+   * @return The hash code as an {@code int}.
+   */
   @Override
   public int hashCode() {
     return Objects.hash(
-        groupOrderID,
-        status,
-        participantOrderIDs,
-        desiredPickupTimeframe,
-        foodProviderID);
+            groupOrderID, status, participantOrderIDs, desiredPickupTimeframe, foodProviderID);
   }
 
+  /**
+   * Converts the GroupOrder object to a string representation.
+   *
+   * @return A string representing the GroupOrder object.
+   */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class GroupOrder {\n");
     sb.append("    groupOrderID: ").append(toIndentedString(groupOrderID)).append("\n");
     sb.append("    status: ").append(toIndentedString(status)).append("\n");
-    sb.append("    participantOrderIDs: ")
-        .append(toIndentedString(participantOrderIDs))
-        .append("\n");
-    sb.append("    desiredPickupTimeframe: ")
-        .append(toIndentedString(desiredPickupTimeframe))
-        .append("\n");
+    sb.append("    participantOrderIDs: ").append(toIndentedString(participantOrderIDs)).append("\n");
+    sb.append("    desiredPickupTimeframe: ").append(toIndentedString(desiredPickupTimeframe)).append("\n");
     sb.append("    foodProviderID: ").append(toIndentedString(foodProviderID)).append("\n");
     sb.append("}");
     return sb.toString();
   }
 
   /**
-   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   * Converts the given object to a string with each line indented by 4 spaces.
+   *
+   * @param o The object to convert to string.
+   *
+   * @return A string representation of the object with each line indented.
    */
   private String toIndentedString(Object o) {
     if (o == null) {
