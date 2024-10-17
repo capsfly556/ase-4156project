dd10e7736625eb5add0a139c62fa240ce00aeca0
Ashley Zhang <ashleyzhang@Ashleys-MacBook-Pro-15.local>
2024-10-16 21:01:26 -0400

    added unit tests for food provider model

--- src/test/java/org/openapitools/foodproviderTest.java
+++ src/test/java/org/openapitools/foodproviderTest.java
@@ -46,5 +46,43 @@ public class foodproviderTest {
         assertEquals(2, foodProvider.getMenu().size());
         assertEquals(menuItem, foodProvider.getMenu().get(1));
     }
+    @Test
+    void testGetters() {
+        assertEquals(id, foodProvider.getFoodProviderID());
+        assertEquals("Test Provider", foodProvider.getName());
+        assertEquals("Test Location", foodProvider.getLocation());
+        assertEquals("123-456-7890", foodProvider.getPhoneNumber());
+        assertEquals("9 AM - 5 PM", foodProvider.getHoursOfOperation());
+        assertEquals(menu, foodProvider.getMenu());
+    }
+
+    @Test
+    void testSetters() {
+        UUID newId = UUID.randomUUID();
+        foodProvider.setName("New Provider");
+        foodProvider.setLocation("New Location");
+        foodProvider.setPhoneNumber("987-654-3210");
+        foodProvider.setHoursOfOperation("8 AM - 6 PM");
+
+        assertEquals("New Provider", foodProvider.getName());
+        assertEquals("New Location", foodProvider.getLocation());
+        assertEquals("987-654-3210", foodProvider.getPhoneNumber());
+        assertEquals("8 AM - 6 PM", foodProvider.getHoursOfOperation());
+    }
+    @Test
+    void testEqualsAndHashCode() {
+        FoodProvider anotherProvider = new FoodProvider()
+                .foodProviderID(id)
+                .name("Test Provider")
+                .location("Test Location")
+                .phoneNumber("123-456-7890")
+                .hoursOfOperation("9 AM - 5 PM")
+                .menu(menu);
+
+        assertEquals(foodProvider, anotherProvider);
+        assertEquals(foodProvider.hashCode(), anotherProvider.hashCode());
+    }
+
+
 
 }
