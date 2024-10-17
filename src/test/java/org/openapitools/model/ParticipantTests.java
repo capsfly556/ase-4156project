705ad3d51cc160b70fd89888bd5c156b88a5168e
Marian Abuhazi <marian.abuhazi@gmail.com>
2024-10-16 21:04:53 -0400

    fixing styling indendation.

--- src/test/java/org/openapitools/model/ParticipantTests.java
+++ src/test/java/org/openapitools/model/ParticipantTests.java
@@ -11,122 +11,122 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParticipantTests {
 
-    private Participant participant;
-    private UUID participantID;
-    private String name;
+  private Participant participant;
+  private UUID participantID;
+  private String name;
 
-    @BeforeEach
-    void setUp() {
-      participantID = UUID.randomUUID();
-      name = "Old name";
-      participant = new Participant();
-      participant.setParticipantID(participantID);
-      participant.setName(name);
-    }
+  @BeforeEach
+  void setUp() {
+    participantID = UUID.randomUUID();
+    name = "Old name";
+    participant = new Participant();
+    participant.setParticipantID(participantID);
+    participant.setName(name);
+  }
 
-    @Test
-    void getParticipantID() {
-      assertEquals(participantID, participant.getParticipantID());
-    }
+  @Test
+  void getParticipantID() {
+    assertEquals(participantID, participant.getParticipantID());
+  }
 
-    @Test
-    void setParticipantID() {
-      UUID newID = UUID.randomUUID();
-      participant.setParticipantID(newID);
-      assertEquals(newID, participant.getParticipantID());
-    }
+  @Test
+  void setParticipantID() {
+    UUID newID = UUID.randomUUID();
+    participant.setParticipantID(newID);
+    assertEquals(newID, participant.getParticipantID());
+  }
 
-    @Test
-    void testGetName() {
-      assertEquals(name, participant.getName());
-    }
+  @Test
+  void testGetName() {
+    assertEquals(name, participant.getName());
+  }
 
-    @Test
-    void testSetName() {
-      String newName = "New name";
-      participant.setName(newName);
+  @Test
+  void testSetName() {
+    String newName = "New name";
+    participant.setName(newName);
 
-      assertEquals(newName, participant.getName());
-    }
+    assertEquals(newName, participant.getName());
+  }
 
-    @Test
-    void testAddParticipantOrder() {
-      ParticipantOrder order = new ParticipantOrder();
-      participant.addParticipantOrder(order);
+  @Test
+  void testAddParticipantOrder() {
+    ParticipantOrder order = new ParticipantOrder();
+    participant.addParticipantOrder(order);
 
-      List<ParticipantOrder> orders = participant.getParticipantOrders();
-      assertEquals(order, orders.get(0));
-      assertEquals(participant, order.getParticipant());
-    }
+    List<ParticipantOrder> orders = participant.getParticipantOrders();
+    assertEquals(order, orders.get(0));
+    assertEquals(participant, order.getParticipant());
+  }
 
-    @Test
-    void setParticipantOrders() {
-      ParticipantOrder order1 = new ParticipantOrder();
-      ParticipantOrder order2 = new ParticipantOrder();
+  @Test
+  void setParticipantOrders() {
+    ParticipantOrder order1 = new ParticipantOrder();
+    ParticipantOrder order2 = new ParticipantOrder();
 
-      List<ParticipantOrder> orders = new ArrayList<>();
-      orders.add(order1);
-      orders.add(order2);
+    List<ParticipantOrder> orders = new ArrayList<>();
+    orders.add(order1);
+    orders.add(order2);
 
-      participant.setParticipantOrders(orders);
+    participant.setParticipantOrders(orders);
 
-      assertEquals(2, participant.getParticipantOrders().size());
-      assertEquals(order1, participant.getParticipantOrders().get(0));
-      assertEquals(order2, participant.getParticipantOrders().get(1));
-    }
+    assertEquals(2, participant.getParticipantOrders().size());
+    assertEquals(order1, participant.getParticipantOrders().get(0));
+    assertEquals(order2, participant.getParticipantOrders().get(1));
+  }
 
-    @Test
-    void testEquals_Success() {
-      assertTrue(participant.equals(participant));
-    }
+  @Test
+  void testEquals_Success() {
+    assertTrue(participant.equals(participant));
+  }
 
-    @Test
-    void testEquals_SuccessDiffObjects() {
-      Participant participant2 = new Participant();
-      participant2.setParticipantID(participantID);
-      participant2.setName(name);
-      participant2.setParticipantOrders(new ArrayList<>());
+  @Test
+  void testEquals_SuccessDiffObjects() {
+    Participant participant2 = new Participant();
+    participant2.setParticipantID(participantID);
+    participant2.setName(name);
+    participant2.setParticipantOrders(new ArrayList<>());
 
-      assertTrue(participant.equals(participant2));
-    }
+    assertTrue(participant.equals(participant2));
+  }
 
-    @Test
-    void testEquals_FailsNullObject() {
-      assertFalse(participant.equals(null));
-    }
+  @Test
+  void testEquals_FailsNullObject() {
+    assertFalse(participant.equals(null));
+  }
 
-    @Test
-    void testEquals_FailsDifferentClass() {
-      assertFalse(participant.equals(new Object()));
-    }
+  @Test
+  void testEquals_FailsDifferentClass() {
+    assertFalse(participant.equals(new Object()));
+  }
 
-    @Test
-    void testEquals_DifferentValues() {
-      Participant participant2 = new Participant();
-      participant2.setParticipantID(UUID.randomUUID());
-      participant2.setName("Different Name");
+  @Test
+  void testEquals_DifferentValues() {
+    Participant participant2 = new Participant();
+    participant2.setParticipantID(UUID.randomUUID());
+    participant2.setName("Different Name");
 
-      assertFalse(participant.equals(participant2));
-    }
+    assertFalse(participant.equals(participant2));
+  }
 
-    @Test
-    void testHashCode_Success() {
-      Participant participant2 = new Participant();
-      participant2.setParticipantID(participantID);
-      participant2.setName(name);
-      participant2.setParticipantOrders(new ArrayList<>());
+  @Test
+  void testHashCode_Success() {
+    Participant participant2 = new Participant();
+    participant2.setParticipantID(participantID);
+    participant2.setName(name);
+    participant2.setParticipantOrders(new ArrayList<>());
 
-      assertEquals(participant.hashCode(), participant2.hashCode());
-    }
+    assertEquals(participant.hashCode(), participant2.hashCode());
+  }
 
-    @Test
-    void testToString_Success() {
-      String expected = "class Participant {\n" +
-        "    participantID: " + participantID + "\n" +
-        "    name: " + name + "\n" +
-        "    participantOrders: []\n" +
-        "}";
+  @Test
+  void testToString_Success() {
+    String expected = "class Participant {\n" +
+      "    participantID: " + participantID + "\n" +
+      "    name: " + name + "\n" +
+      "    participantOrders: []\n" +
+      "}";
 
-      assertEquals(expected, participant.toString());
-    }
+    assertEquals(expected, participant.toString());
+  }
 }
