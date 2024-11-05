255e62c3275d2683b2a2c70dad6986a271a45a07
Zhengwen Fu <zfmyac@gmail.com>
2024-11-05 01:08:51 -0500

    Modify Participant POST

--- src/main/java/org/openapitools/repository/ParticipantRepository.java
+++ src/main/java/org/openapitools/repository/ParticipantRepository.java
@@ -1,11 +1,16 @@
 package org.openapitools.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.openapitools.model.Participant;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ParticipantRepository extends JpaRepository<Participant, UUID> {}
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+    @Query("SELECT u FROM Participant u WHERE u.name = ?1")
+    Optional<Participant> findUserByName(String name);
+}
